e08563751659daaac88e05ddd97e34560a67cfb5
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-07-24 09:14:37 +0530

    update cors

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,12 @@ cloudinary.config({
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 
